113fb99402ed40b163b01b7e0fe08b5ff096b6f7
Misha Moroshko <michael.moroshko@gmail.com>
2025-04-15 15:29:06 +1000

    Fix build

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,15 @@
 import { redirect } from "next/navigation";
+import { Suspense } from "react";
 
-export default async function Home() {
+const HomeContent = async () => {
   await new Promise((resolve) => setTimeout(resolve, 1000));
   
   redirect("/dashboard");
+
+}
+
+export default async function Home() {
+  return <Suspense fallback={<div>Loading...</div>}>
+    <HomeContent />
+  </Suspense>
 }
