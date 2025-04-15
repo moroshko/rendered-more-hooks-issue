f58fdaa268f589a7dbe7ed9f31ba481b8b5f9e8c
Misha Moroshko <michael.moroshko@gmail.com>
2025-04-15 15:36:04 +1000

    Read headers

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
+import { headers } from "next/headers";
 import { redirect } from "next/navigation";
 import { Suspense } from "react";
 
 const HomeContent = async () => {
-  await new Promise((resolve) => setTimeout(resolve, 1000));
-  
-  redirect("/dashboard");
+  const headersList = headers();
+  const newPage = await new Promise<string>((resolve) => {
+    setTimeout(() => {
+      resolve("/dashboard");
+    }, 1000);
+  });
 
+  redirect(newPage);
 }
 
 export default async function Home() {
