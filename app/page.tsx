6dfaaf0cc922b234c7c9d0032f578216e518d59e
Misha Moroshko <michael.moroshko@gmail.com>
2025-04-22 15:09:55 +1000

    Add Suspense in layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,5 @@
-import { headers } from "next/headers";
 import { redirect } from "next/navigation";
-import { Suspense } from "react";
-
-const HomeContent = async () => {
-  const headersList = headers();
-  const newPage = await new Promise<string>((resolve) => {
-    setTimeout(() => {
-      resolve("/dashboard");
-    }, 1000);
-  });
-
-  redirect(newPage);
-}
 
 export default async function Home() {
-  return <Suspense fallback={<div>Loading...</div>}>
-    <HomeContent />
-  </Suspense>
+  redirect("/dashboard");
 }
